4f0a6dec0e912fba2db5c8e88a1cd7510cd68d61
Amelie Pol <pol.amelie@gmail.com>
2020-10-09 17:51:35 +0200

    email reminder

--- lib/tasks/mailer_scheduler.rb
+++ lib/tasks/mailer_scheduler.rb
@@ -1,16 +1,16 @@
-# class MailerScheduler < ParticipantMailer
+class MailerScheduler < ParticipantMailer
 
-#   namespace :mailer_scheduler do
-#     desc "This task is called by the Heroku scheduler add-on to send reminder mail to participants"
-#     task :send_emails_to_participants_before_event => :environment do
-#       alert_one_month_before = (DateTime.now - 2).to_date
-#       alert_fifteen_days_before = (DateTime.now - 1).to_date
-#       alert_seven_days_before = (DateTime.now - 1).to_date
-#       alert_one_day_before = (DateTime.now - 1).to_date
-#       participants = Participants.all
-#       participants.each do |participant|
-#         ParticipantMailer.reminder(participant).deliver_now
-#       end
-#     end
-#   end
-# end
+  namespace :mailer_scheduler do
+    desc "This task is called by the Heroku scheduler add-on to send reminder mail to participants"
+    task :send_emails_to_participants_before_event => :environment do
+      alert_one_month_before = (DateTime.now - 2).to_date
+      alert_fifteen_days_before = (DateTime.now - 1).to_date
+      alert_seven_days_before = (DateTime.now - 1).to_date
+      alert_one_day_before = (DateTime.now - 1).to_date
+      participants = Participants.all
+      participants.each do |participant|
+        ParticipantMailer.reminder(participant).deliver_now
+      end
+    end
+  end
+end
