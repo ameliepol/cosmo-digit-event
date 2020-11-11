925766f60edb74a26dfd2ee09a86499f4537edf1
Amelie Pol <pol.amelie@gmail.com>
2020-11-12 00:20:09 +0100

    dashboard company admin ok

--- app/controllers/company_admin/dashboard_controller.rb
+++ app/controllers/company_admin/dashboard_controller.rb
@@ -2,11 +2,13 @@ class CompanyAdmin::DashboardController < ApplicationController
 
   def show
     # @user = current_user
-    # @bookings = Booking.includes(:workshop).where(workshop: {event: @event})
+    @bookings = Booking.includes(:workshop).where(workshop: {event: @event})
     @participants = Participant.includes(:bookings).where(bookings: {status: "confirmed"})
     @event = Event.last
     @workshops = @event.workshops.visibles
+    @bookings = Booking.includes(:workshop)
     @questions = Question.all
+
 
     respond_to do |format|
       format.html
