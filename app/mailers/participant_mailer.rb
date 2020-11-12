2846a5448d11962a3ea662aa0131c02567bd857b
Amelie Pol <pol.amelie@gmail.com>
2020-11-12 23:51:42 +0100

    init question mailer

--- app/mailers/participant_mailer.rb
+++ app/mailers/participant_mailer.rb
@@ -3,6 +3,19 @@ class ParticipantMailer < ApplicationMailer
   def confirmation(participant)
     @participant = participant
     @bookings = @participant.bookings
+    @bookings_by_date = @bookings.group_by{|b| b.workshop.start_at.to_date}
+    attachments.inline['logo_site_erasmus.png'] = File.read('app/assets/images/logo_site_erasmus.png')
+    mail(to: @participant.email, subject: "Erasmus+ - Confirmation d'inscription") do |format|
+      format.html { render(layout: 'mailer') }
+    end
+  end
+
+  def question(participant)
+    @event = Event.find(params[:event_id])
+    @question = Question.find(params[:id])
+    @participant = participant
+    @bookings = @participant.bookings
+    @bookings_by_date = @bookings.group_by{|b| b.workshop.start_at.to_date}
     attachments.inline['logo_site_erasmus.png'] = File.read('app/assets/images/logo_site_erasmus.png')
     mail(to: @participant.email, subject: "Erasmus+ - Confirmation d'inscription") do |format|
       format.html { render(layout: 'mailer') }
