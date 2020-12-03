6b731d9c1469b9ae482076f5c70a5d1120f49d7b
Amelie Pol <pol.amelie@gmail.com>
2020-12-03 10:00:14 +0100

    program updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,26 +24,19 @@ workshop1.update(description: "Madame Laure Coudret-Laut, directrice de l’Agen
                               Madame Thémis Christophidou, directrice générale éducation, culture, jeunesse et sports, Commission européenne\n
                               Monsieur Bernard Beignier, recteur de la région académique Provence-Alpes-Côte-D’azur, recteur de l’académie d’Aix-Marseille")
 workshop2 = Workshop.find_by(name: "Erasmus + 2021-2027 : nouvel élan, nouvelles opportunités")
-workshop2.update(description: "Présentation générale de ce nouveau programme, plus ouvert, plus responsable et plus ambitieux.Cette séquence s’adresse à tous
-                 les acteurs de l’éducation, de la formation, de la jeunesse et du sport.")
+workshop2.update(description: "Présentation générale de ce nouveau programme, plus ouvert, plus responsable et plus ambitieux. Cette séquence s’adresse à tous les acteurs de l’éducation, de la formation, de la jeunesse et du sport.")
 workshop3 = Workshop.find_by(name: "S’informer pour bien débuter avec Erasmus + 2021-2027")
-workshop3.update(description: "Présentation orientée conseils et outils pour démarrer efficacement son futur projet de mobilités ou de coopération. Cette séquence
-                 s’adresse aux acteurs de l’éducation et de la formation qui veulent se lancer dans Erasmus +.")
+workshop3.update(description: "Présentation orientée conseils et outils pour démarrer efficacement son futur projet de mobilités ou de coopération. Cette séquence s’adresse aux acteurs de l’éducation et de la formation qui veulent se lancer dans Erasmus +.")
 workshop5 = Workshop.find_by(name: "Acteurs de l’enseignement scolaire / Focus sur les opportunités Erasmus + 2021-2027")
-workshop5.update(description: "Cette session s’adresse aux participants qui souhaitent approfondir les nouveautés des projets de mobilités ou de coopération du
-                 secteur de l’enseignement scolaire (de la petite enfance jusqu’au lycée général et/ou technologique).")
+workshop5.update(description: "Cette session s’adresse aux participants qui souhaitent approfondir les nouveautés des projets de mobilités ou de coopération du secteur de l’enseignement scolaire (de la petite enfance jusqu’au lycée général et/ou technologique).")
 workshop6 = Workshop.find_by(name: "Acteurs de l’Enseignement et de la Formation Professionnels / Focus sur les opportunités Erasmus + 2021-2027")
-workshop6.update(description: "Cette session s’adresse aux participants qui souhaitent approfondir les nouveautés des projets de mobilités ou de coopération du
-                 secteur l’Enseignement et de la Formation Professionnels (lycée professionnel, CFA, organisme de formation et d’insertion, …).")
+workshop6.update(description: "Cette session s’adresse aux participants qui souhaitent approfondir les nouveautés des projets de mobilités ou de coopération du secteur de l’Enseignement et de la Formation Professionnels (lycée professionnel, CFA, organisme de formation et d’insertion, …).")
 workshop7 = Workshop.find_by(name: "Acteurs de l’Enseignement supérieur / Focus sur les opportunités Erasmus + 2021-2027")
-workshop7.update(description: "Cette session s’adresse aux participants qui souhaitent approfondir les nouveautés des projets de mobilités ou de coopération du
-                 secteur de l’enseignement supérieur (universités, écoles, instituts, lycées avec STS, CFA du supérieur, …)")
-workshop8 = Workshop.find_by(name: name: "Acteurs du secteur de la jeunesse / Focus sur les opportunités Erasmus + 2021-2027")
-workshop8.update(description: "Cette session s’adresse aux participants qui souhaitent approfondir les nouveautés des projets de mobilités ou de coopération du
-                 secteur de la jeunesse")
+workshop7.update(description: "Cette session s’adresse aux participants qui souhaitent approfondir les nouveautés des projets de mobilités ou de coopération du secteur de l’enseignement supérieur (universités, écoles, instituts, lycées avec STS, CFA du supérieur, …)")
+workshop8 = Workshop.find_by(name: "Acteurs du secteur de la jeunesse / Focus sur les opportunités Erasmus + 2021-2027")
+workshop8.update(description: "Cette session s’adresse aux participants qui souhaitent approfondir les nouveautés des projets de mobilités ou de coopération du secteur de la jeunesse")
 workshop9 = Workshop.find_by(name: "Acteurs de l’Education des Adultes / Focus sur les opportunités Erasmus + 2021-2027")
-workshop9.update(description: "Cette session s’adresse aux participants qui souhaitent approfondir les nouveautés des projets de mobilités ou de coopération du
-                 secteur de l’éducation des adultes (économie sociale et solidaire, éducation populaire, action sociale, …).")
+workshop9.update(description: "Cette session s’adresse aux participants qui souhaitent approfondir les nouveautés des projets de mobilités ou de coopération du secteur de l’éducation des adultes (économie sociale et solidaire, éducation populaire, action sociale, …).")
 
 workshop10 = Workshop.find_by(name: "Enseignement scolaire")
 workshop10.update(description: "Vos questions, nos réponses")
