d59d45fd5dfc506bca141ddf44ebe8396cc0ddc9
Amelie Pol <pol.amelie@gmail.com>
2021-04-26 12:19:24 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,36 @@ workshop_grdr3 = Workshop.create!(start_at: DateTime.new(2021,01,14,11.5), end_a
 description: "Échange sur l’historique et les tendances des migrations et mobilités sous régionales et internationales, les dynamiques de partage et gestion concertée des ressources, les facteurs de conflits et tensions, et les notions d’autochtone et allochtone dans un territoire partageant des ressources communes et des liens historiques.", event: event_grdr)
 puts "Workshops created..."
 
+puts "Updating workshops"
+workshop1 = Workshop.find_by(name: "Bilan du programme actuel et nouvelles perspectives et défis pour Erasmus+ 2021-2027")
+workshop1.update(visible: false)
+workshop2 = Workshop.find_by(name: "Erasmus + 2021-2027 : nouvel élan et nouvelles opportunités")
+workshop2.update(visible: false)
+workshop3 = Workshop.find_by(name: "S’informer pour bien débuter avec Erasmus + 2021-2027")
+workshop3.update(visible: false)
+workshop5 = Workshop.find_by(name: "Acteurs de l’enseignement scolaire / Focus sur les opportunités Erasmus + 2021-2027")
+workshop5.update(visible: false)
+workshop6 = Workshop.find_by(name: "Acteurs de l’Enseignement et de la Formation Professionnels / Focus sur les opportunités Erasmus + 2021-2027")
+workshop6.update(visible: false)
+workshop7 = Workshop.find_by(name: "Acteurs de l’Enseignement supérieur / Focus sur les opportunités Erasmus + 2021-2027")
+workshop7.update(visible: false)
+workshop8 = Workshop.find_by(name: "Acteurs du secteur de la jeunesse et du sport / Focus sur les opportunités Erasmus + 2021-2027")
+workshop8.update(visible: false)
+workshop9 = Workshop.find_by(name: "Acteurs de l’Education des Adultes / Focus sur les opportunités Erasmus + 2021-2027")
+workshop9.update(visible: false)
+
+workshop10 = Workshop.find_by(name: "Enseignement scolaire")
+workshop10.update(visible: false)
+workshop11 = Workshop.find_by(name: "Enseignement & formations professionnels")
+workshop11.update(visible: false)
+workshop12 = Workshop.find_by(name: "Enseignement supérieur")
+workshop12.update(visible: false)
+workshop13 = Workshop.find_by(name: "Secteur jeunesse et sport")
+workshop13.update(visible: false)
+workshop14 = Workshop.find_by(name: "Education des adultes")
+workshop14.update(visible: false)
+puts "Workshops updated..."
+
 puts "Creating participants..."
 participant_grdr1 = Participant.create!(email: "amelie@agencecosmo.com", company: "Agence Cosmo", first_name: "Amélie", last_name: "Pol", position: "Directrice", organization: "Agence Cosmo", accepted_conditions: true)
 puts "Participants created..."
