b5e23c579c0462d1e0225b3557c5bd76a0c22b5f
Amelie Pol <pol.amelie@gmail.com>
2021-04-27 09:50:49 +0200

    set 404 422 and 500 pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,9 +7,10 @@ class PagesController < ApplicationController
 
   def programme
     set_active_navbar_link(3)
-    workshops_grouped = Workshop.order(start_at: :asc).group_by{|w| w.start_at.to_date}.values
+    workshops_grouped = Workshop.visibles.order(start_at: :asc).group_by{|w| w.start_at.to_date}.values
     @workshops_1 = workshops_grouped[0]
     @workshops_2 = workshops_grouped[1]
+    @workshops_3 = workshops_grouped[2]
   end
 
   def inscription
--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -3,22 +3,26 @@ class ParticipantsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def show
-    @event = Event.find(params[:event_id])
+    # @event = Event.find(params[:event_id])
+    @event = Event.last
     @participant = Participant.find(params[:id])
     @bookings = @participant.bookings
     @bookings_by_date = @bookings.group_by{|b| b.workshop.start_at.to_date}
   end
 
   def new
-    @event = Event.find(params[:event_id])
+    # @event = Event.find(params[:event_id])
+    @event = Event.last
     @participant = Participant.new
     workshops_grouped = Workshop.visibles.order(start_at: :asc).group_by{|w| w.start_at.to_date}.values
     @workshops_1 = workshops_grouped[0]
     @workshops_2 = workshops_grouped[1]
+    @workshops_3 = workshops_grouped[2]
   end
 
   def create
-    @event = Event.find(params[:event_id])
+    # @event = Event.find(params[:event_id])
+    @event = Event.last
     @participant = Participant.new(participant_params)
     if @participant.save
       redirect_to event_participant_path(@event, @participant)
@@ -26,6 +30,7 @@ class ParticipantsController < ApplicationController
       workshops_grouped = Workshop.visibles.order(start_at: :asc).group_by{|w| w.start_at.to_date}.values
       @workshops_1 = workshops_grouped[0]
       @workshops_2 = workshops_grouped[1]
+      @workshops_3 = workshops_grouped[2]
       flash[:alert] = @participant.errors.full_messages.join("; ")
       @workshops = @event.workshops.visibles
       render :new
