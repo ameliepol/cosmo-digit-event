c3f82437e920bc92e0da6ce8d9cde15396240ccd
Amelie Pol <pol.amelie@gmail.com>
2021-04-29 12:37:46 +0200

    updated participant form

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ require 'date'
 
 puts "Updating users..."
 user1 = User.find_by(email: "amelie@agencecosmo.com")
-# user1.update(password: "CosmoEvent")
-user2 = User.find_by(email: "chloe@agencecosmo.com")
+user1.update(password: "CosmoEvent")
+# user2 = User.find_by(email: "chloe@agencecosmo.com")
 # user2.update(password: "CosmoEvent")
 puts "Users updated..."
 
