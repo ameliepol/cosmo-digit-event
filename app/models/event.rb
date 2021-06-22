cdb496c11fe6d6b2588a537d9e7d5c5d58bc606b
Amelie Pol <pol.amelie@gmail.com>
2021-06-22 10:39:18 +0200

    update modal

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,8 +7,6 @@ class Event < ApplicationRecord
   validates :start_at, presence: true
   validates :end_at, presence: true
   validates :name, presence: true
-  # validates :place, presence: true
-  # validates :description, presence: true
 
   enum emailing_status: { confirmation: 0, alert_one_month_before: 1, alert_fifteen_days_before: 2, alert_seven_days_before: 3, alert_one_day_before: 4 }
 end
