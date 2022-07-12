9716eb1153f4d4d72be0670584d8802a83c50fe4
Amelie Pol <pol.amelie@gmail.com>
2022-07-13 00:04:16 +0200

    update seed

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,8 +5,8 @@ class Event < ApplicationRecord
   has_many :questions
 
   validates :start_at, presence: true
-  validates :end_at, presence: true
-  validates :name, presence: true
+  # validates :end_at, presence: true
+  # validates :name, presence: true
 
   enum emailing_status: { confirmation: 0, alert_one_month_before: 1, alert_fifteen_days_before: 2, alert_seven_days_before: 3, alert_one_day_before: 4 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,6 @@ Workshop.destroy_all
 Participant.destroy_all
 User.destroy_all
 Event.destroy_all
-DateTime.destroy_all
 puts "DB destroyed!"
 
 puts "Creating users..."
@@ -17,12 +16,13 @@ user2 = User.create!(email: "chloe@agencecosmo.com", password: "Aquilogia2022", 
 user3 = User.create!(email: "c.lamaison@aquilogia.fr", password: "Aquilogia2022", company: "Aquilogia", admin: false, first_name: "Camille", last_name: "Lamaison")
 puts "Users created..."
 
-puts "Creating dates..."
-start_date1 = DateTime.new(2022,09,08)
-end_date1 = DateTime.new(2022,09,09)
-puts "Dates created..."
+# puts "Creating dates..."
+# start_date1 = DateTime.new(2022,09,08)
+# end_date1 = DateTime.new(2022,09,09)
+# puts "Dates created..."
 
 puts "Creating events..."
-event1 = Event.create!(start_at: start_date1, end_at: end_date1, place: "Saint-Jean-de-Luz", name: "Inauguration Aquilogia Saint-Jean-de-Luz", user_id: user1.id)
+event1 = Event.create!(place: "Saint-Jean-de-Luz", name: "Inauguration Aquilogia Saint-Jean-de-Luz", user_id: user1.id)
+#event1 = Event.create!(start_at: start_date1, end_at: end_date1, place: "Saint-Jean-de-Luz", name: "Inauguration Aquilogia Saint-Jean-de-Luz", user_id: user1.id)
 puts "Events created..."
 
