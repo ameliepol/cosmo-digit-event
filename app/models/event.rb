cd250273c4642d48ef6962cd11078521eb1b421f
Amelie Pol <pol.amelie@gmail.com>
2022-07-13 00:06:07 +0200

    update seed

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,9 +4,9 @@ class Event < ApplicationRecord
   has_many :bookings
   has_many :questions
 
-  validates :start_at, presence: true
+  # validates :start_at, presence: true
   # validates :end_at, presence: true
-  # validates :name, presence: true
+  validates :name, presence: true
 
   enum emailing_status: { confirmation: 0, alert_one_month_before: 1, alert_fifteen_days_before: 2, alert_seven_days_before: 3, alert_one_day_before: 4 }
 end
